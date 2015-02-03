8610b3e26563107fb3ff7a42e257741f5580d0b1
dsmith31 <daniel.T.smith@gtri.gatech.edu>
2015-02-03 16:14:46 -0500

    Initial Commit

---
+++ TriangleType/ArrayProblem/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ArrayProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> testList = new List<int> {1, 2, 3, 8, 5, 6};
+            Console.WriteLine(GetNthFromLast(testList,4));
+            Console.WriteLine(CantCountSolution(testList, 4));
+            
+            Console.ReadLine();
+        }
+
+
+        private static string CantCountSolution(List<int> list, int nth=5)
+        {
+            if (nth <= 0)
+            {
+                return "Invalid Input: Please enter an integer greater than '0'";
+            }
+            int lastEntry = 0;
+            while (true)
+            {
+                try
+                {
+                    int placeholder = (list[lastEntry]);     //operation that will cause us to hit the exeption and fall out once we get 
+                    lastEntry = lastEntry + nth;             //to an index longer that the length of the list
+                }
+                catch (Exception e)
+                {
+                    break;                                   
+                }
+            }
+
+            lastEntry--;
+
+            for (int i = 0; i < nth; i++)
+            {
+                try
+                {
+                    int placeholder = (list[lastEntry]);
+                    if (lastEntry < nth-1)
+                    {
+                        break;
+                    }
+                }
+                catch (Exception e)
+                {
+                    lastEntry--;             //every time we go beyond the list index we hit an exception that decriments the lastEntry
+                }
+            }
+            
+            return list[lastEntry - (nth-1)].ToString();
+        }
+
+
+        private static string GetNthFromLast(List<int> list, int nth=5)
+        {
+            
+
+            ConcreteQueue<int> myQueue = new ConcreteQueue<int> {maxSize = nth};
+            if (nth <= 0)
+            {
+                return "Invalid Input: Please enter an integer greater than '0'";
+            }
+            int result;
+            if (list.Count() < nth)
+            {
+                throw new Exception("List not long enough");
+            }
+            foreach (int entry in list)
+            {
+                myQueue.Enqueue(entry);
+            }
+            myQueue.queue.TryPeek(out result);
+            
+            return result.ToString();
+        }
+    }
+}
---
+++ TriangleType/TriangleType/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TriangleType
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(triType(0, 2, 2));
+            Console.ReadLine();
+        }
+
+        static string triType(int side1, int side2, int side3)
+        {
+            List<int> sides = new List<int> {side1, side2, side3};
+            string triangleType = "Error: Invalid side lengths";
+            if (sides.Any(x=>x<1))
+            {
+                return triangleType;
+            }
+            if (sides.Distinct().Count() == 1)
+            {
+                return "Equilateral";
+            }
+            if (sides.Distinct().Count() == 2)
+            {
+                return "Isosceles";
+            }
+            if (sides.Distinct().Count() == 3)
+            {
+                return "Scalene";
+            }
+            return triangleType;
+        }
+    }
+}
