58212be659e8f05e7c3332e0bda3dfa4d88bf082
gtg985r <gtg985r@mail.gatech.edu>
2015-02-05 01:19:42 -0500

    test class added for prime factorization

---
+++ TriangleType/PrimeFactorizationTestClass/TestClass.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using PrimeFactorization;
+
+namespace PrimeFactorizationTestClass
+{
+    [TestFixture]
+    public class TestClass
+    {
+        [Test]
+        public void primeFactorTest1()
+        {
+            string actual = FunctionRepository.BuildOutputString(28);
+            string expected = "2,2,7 Product: 28";
+            Assert.AreEqual(actual, expected);
+        }
+    }
+}
